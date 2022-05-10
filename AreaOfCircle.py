f7e25a16edb8b15eefe64b14c7ba9b238df9f18e
abinash1219 <76238920+abinash1219@users.noreply.github.com>
2022-05-10 20:39:07 +0530

    Update AreaofCircle.py

--- AreaOfCircle.py
+++ AreaOfCircle.py
@@ -1,12 +1,8 @@
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
 
 
-from math import pi
- 
 
-r = float(input ("Input the radius of the circle : "))
- 
 
-calculateArea = str(pi * r**2);
- 
-
-print ("The area of the circle with radius " + str(r) + " is: " + calculateArea)
